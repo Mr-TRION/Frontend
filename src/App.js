b569616d7f52e9153b348291ac3ba20f35df413a
prakashkshaw <prakashshawin2050@gmail.com>
2024-05-16 18:30:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -299,7 +299,7 @@ function App() {
     const res = await axios.get(`https://backend-d7ys.onrender.com/filter/${Education}/${marks}/${cat}/${sport}/${Dis}/${inc}/${sex}/${service}`);
 
     console.log(res);
-    setResults([]);
+    setResults();
       // setsearchName(search);
     setResults(res.data);
     // console.log(cat);
